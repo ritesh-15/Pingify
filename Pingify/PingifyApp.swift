e015ca8e0128679b0061abf2783da889f782c720
Ritesh Khore <riteshkhore@gmail.com>
2026-03-07 22:29:23 +0530

    Initial Commit

---
+++ Pingify/PingifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingifyApp.swift
+//  Pingify
+//
+//  Created by Ritesh Khore on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PingifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
